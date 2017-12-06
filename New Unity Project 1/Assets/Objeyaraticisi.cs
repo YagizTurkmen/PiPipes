70a3e51587898a553e8e2a6aa30348d935a1ff35
Kutayarkan <kutayarkan@sabanciuniv.edu>
2017-12-06 17:40:35 +0300

    6.12.17
    Box collider kod ile yazildi (baslangic)

---
+++ New Unity Project 1/Assets/Objeyaraticisi.cs
@@ -0,0 +1,104 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Objeyaraticisi : MonoBehaviour {
+
+
+	static GameObject SObjesi;
+
+	float y = 0.5f;  //daire icindeki kare
+	float z = 2.0f;	//degisken
+	float z_ust;
+	float z_alt;
+	float dilimgenisligi = 0.025f; //dilimlerin genişliği
+
+
+	float xcenter = 0.975f; 
+	float ycenter = 0.31f;
+	float zcenter;
+
+	// Use this for initialization
+	void Start () {
+
+		SObjesi = gameObject.transform.GetChild (0).gameObject;
+
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+
+
+			while(xcenter > 0.25f){
+			
+
+
+
+
+			z_ust = Mathf.Sqrt (1.25f*1.25f - (xcenter -1.0f - dilimgenisligi/2) * (xcenter -1.0f - dilimgenisligi/2))+1;
+			z_ust = -1 * z_ust;
+
+			z_alt = Mathf.Sqrt (0.75f*0.75f - (xcenter -1.0f + dilimgenisligi/2) * (xcenter -1.0f + dilimgenisligi/2))+1;
+			z_alt = -1 * z_alt;
+			z = (float) (z_ust - z_alt);
+
+			zcenter = (float)(z_ust + z_alt) / 2;
+			Debug.Log ("z_ust = " +z_ust);
+			Debug.Log ("z_alt = " +z_alt);
+
+			Debug.Log ("zcenter = " +zcenter);
+
+
+
+			BoxCollider bc = SObjesi.AddComponent<BoxCollider> () as BoxCollider;
+			bc.center = new Vector3(xcenter,ycenter,zcenter);
+			bc.size = new Vector3(dilimgenisligi,y,z);
+
+
+
+
+			xcenter = xcenter - dilimgenisligi;
+
+	
+
+			}
+
+
+		while(xcenter > -0.2499f){
+
+
+
+
+
+			z_ust = Mathf.Sqrt (1.25f*1.25f - (xcenter -1.0f - dilimgenisligi/2) * (xcenter -1.0f - dilimgenisligi/2))+1;
+			z_ust = -1 * z_ust;
+
+			z_alt = -1;
+			z = (float) (z_ust - z_alt);
+
+			zcenter = (float)(z_ust + z_alt) / 2;
+			Debug.Log ("z_ust = " +z_ust);
+			Debug.Log ("z_alt = " +z_alt);
+
+			Debug.Log ("zcenter = " +zcenter);
+
+
+
+			BoxCollider bc = SObjesi.AddComponent<BoxCollider> () as BoxCollider;
+			bc.center = new Vector3(xcenter,ycenter,zcenter);
+			bc.size = new Vector3(dilimgenisligi,y,z);
+
+
+
+
+			xcenter = xcenter - dilimgenisligi;
+
+
+
+		}
+		}
+		
+	}
+
